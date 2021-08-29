b3b889e22e98f354fd66654b27646c71f786a9c6
Jatin <negijatin2106@gmail.com>
2021-08-29 14:00:00 +0100

    eptcommit

---
+++ Practice/Function/Fibenacci_Sequence.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+void Fib(int n){
+    int t1=0;
+    int t2=1;
+    int next_Term;
+
+    for(int i=1; i<=n;i++){
+        cout<<t1<<endl;
+        next_Term=t1+t2;
+        t1=t2;
+        t2=next_Term;
+    }
+    return ;
+
+}
+
+int main(){
+    int n;
+    cin>>n;
+
+    Fib(n);
+    
+
+    return 0;
+}
