cf9b8e87f91fd55029232b49b6121a9e4c6b1a90
Jatin <negijatin2106@gmail.com>
2021-08-31 17:43:58 +0530

    450DSA

---
+++ Practice/450 DSA/Reverse_the_array.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    int s=0;
+    int e=n-1;
+
+    while (s<e)
+    {
+        int temp=arr[s];
+        arr[s]=arr[e];
+        arr[e]=temp;
+        s++;
+        e--;
+    }
+
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+
+}
