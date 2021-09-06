65006ff23423f077877803e7745d739083d74aef
Jatin <negijatin2106@gmail.com>
2021-09-06 10:57:24 +0530

    Character Array

---
+++ Practice/Array/Character array/Check_palindrone.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    char arr[n+1];
+    cin >> arr;
+
+    bool check = 1;
+
+    for(int i=0; i<n; i++){
+        if(arr[i] != arr[n-1-i]){
+            check = 0;
+            break;
+        }
+    }
+    if(check == true){
+        cout<<" It is a palindrone"<<endl;
+    }
+    else{
+    cout<<"It is not a palindrone"<<endl;
+    }
+
+    return 0;
+}
