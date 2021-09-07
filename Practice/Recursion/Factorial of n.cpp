84b094db6164168e901124f6831941ebf52fd874
Jatin <negijatin2106@gmail.com>
2021-09-07 16:17:42 +0530

    Recursion

---
+++ Practice/Recursion/Factorial of n.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int Factorial(int n)
+{
+    if( n==0)
+    {
+        return 1;
+    }
+    int prevFactorial=Factorial(n-1);
+
+    return n*prevFactorial;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    cout<<Factorial(n)<<endl;
+
+    return 0;
+}
