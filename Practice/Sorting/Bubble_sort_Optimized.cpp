3e6d60788d5cd13cef3409ee06eba00333491614
Jatin <negijatin2106@gmail.com>
2021-09-11 09:08:30 +0530

    sorting

---
+++ Practice/Sorting/Bubble_sort_Optimized.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void Bubblesort(int arr[],int n)
+{
+    bool swapped;
+    for(int i=0; i<n; i++)
+    {
+
+        swapped = false;
+        for(int j=0; j<n-i-1; j++)
+        {
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+                swapped = true;
+            }
+        }
+         
+         if(swapped = false)
+          break;
+    }
+
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    int arr[n];
+    
+   for(int i=0; i<n; i++){
+       cin>>arr[i];
+   }
+
+   Bubblesort(arr,n);
+
+   for(int i=0; i<n; i++){
+       cout<<arr[i]<<" ";
+   }
+
+   return 0;
+
+
+}
